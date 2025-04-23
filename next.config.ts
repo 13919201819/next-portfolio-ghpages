f1f8c64b777ff1ce57d22c5784902becf089ab08
Karan Bhatt <186667142+13919201819@users.noreply.github.com>
2025-04-24 00:29:28 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? '/next-portfolio-ghpages' : '',
+  assetPrefix: isProd ? '/next-portfolio-ghpages/' : '',
 };
 
 export default nextConfig;
