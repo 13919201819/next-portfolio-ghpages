23eef554c4f72a60ba09ee0831f5294d8d181330
Karan Bhatt <186667142+13919201819@users.noreply.github.com>
2025-04-29 23:05:49 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/3122113151919-Updated-", // Must match your GitHub repo name
+  basePath: "/next-portfolio-ghpages", // Must match your GitHub repo name
   trailingSlash: true,
   images: {
     unoptimized: true,
