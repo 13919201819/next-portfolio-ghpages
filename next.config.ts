224772f2d7216be6210c01f86b7832ea383d8e2e
Karan Bhatt <186667142+13919201819@users.noreply.github.com>
2025-05-04 11:57:42 +0530

    hello

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,34 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+// const nextConfig: NextConfig = {
+//   output: "export",
+//   basePath: "/next-portfolio-ghpages", // Must match your GitHub repo name
+//   trailingSlash: true,
+//   images: {
+//     unoptimized: true,
+//   },
+
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+// export default nextConfig;
+
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
   output: "export",
-  basePath: "/next-portfolio-ghpages", // Must match your GitHub repo name
+  basePath: isProd ? "/next-portfolio-ghpages" : "",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
 export default nextConfig;
+
